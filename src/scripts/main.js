8a8548d368fcac95a61cf82285896bcc6ee1c11b
Helen Petrovska <paznachka@gmail.com>
2022-07-05 17:47:31 +0300

    widget was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const message = document.querySelector('.message');
+const avatar = document.querySelector('.avatar');
+
+function openAvatar() {
+  avatar.classList.add('avatar--show');
+  avatar.classList.remove('avatar--hide');
+}
+
+function openMessage() {
+  message.classList.add('message--show');
+  message.classList.remove('message--hide');
+}
+
+function closeMessage() {
+  message.classList.add('message--hide');
+  message.classList.remove('message--show');
+}
+
+setTimeout(openAvatar, 2000);
+setTimeout(openMessage, 5000);
+setTimeout(closeMessage, 10000);
